694197db6125dc11d6648d4d73a908e2627940d1
Gustaf Hultgren <gustaf.hultgren@byon8.com>
2017-10-31 10:00:53 +0100

    cards route set up

--- app.js
+++ app.js
@@ -4,54 +4,28 @@ const cookieparser = require('cookie-parser');
 // const pug = require('pug');
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(cookieparser());
 
-const colors = [
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'purple'
-  ];
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
 
-app.set('view engine', 'pug');
-
-app.get('/', function(req, res){
-    const name = req.cookies.username;
-    if (name) {
-        res.render('index', {name});
-    } else {
-        res.redirect('/hello');
-    }
-});
-
-app.get('/hello', function(req, res){
-    const name = req.cookies.username;
-    if (name) {
-        res.redirect('/')
-    } else {
-        res.render('hello');
-    }
-});
-
-app.post('/hello', function(req, res){
-    res.cookie('username', req.body.username);
-    res.redirect('/hello');
-});
-
-app.post('/goodbye', function(req, res){
-    res.clearCookie('username');
-    res.redirect('/hello');
-});
-
-app.get('/cards', function (req, res) {
-    // res.locals.prompt = "Who let the dogs out?";
-    res.render('cards', {prompt: 'Who is buried in Grant\'s tomb?',
-    hint: 'think about whose tomb it is',
-    colors: colors});
+app.use(function(req, res, next){
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 })
+
+app.use(function(err, req, res, next){
+    console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+});
 
 app.listen(3000, function(){
     console.log('server listening on port 3000');
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res){
+    const name = req.cookies.username;
+    if (name) {
+        res.render('index', {name});
+    } else {
+        res.redirect('/hello');
+    }
+});
+
+router.get('/hello', function(req, res){
+    const name = req.cookies.username;
+    if (name) {
+        res.redirect('/')
+    } else {
+        res.render('hello');
+    }
+});
+
+router.post('/hello', function(req, res){
+    res.cookie('username', req.body.username);
+    res.redirect('/hello');
+});
+
+router.post('/goodbye', function(req, res){
+    res.clearCookie('username');
+    res.redirect('/hello');
+});
+
+
+module.exports = router;
